1ce33695b60715555b91e58ff151332f375a4a02
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2022-09-08 10:40:41 -0300

    Update atv 1.cpp

--- lista1/atv 1.cpp
+++ lista1/atv 1.cpp
@@ -1,2 +1,19 @@
 /*Construa um algoritmo que leia um número inteiro de horas e mostre ao usuário o
 correspondente em minutos e segundos destas horas.*/
+
+#include<stdio.h>
+
+int main (){
+int hora, minutos, segundos;
+
+printf("digite o horario que deseja converter em horas: ");
+scanf("%i", &hora);
+
+minutos = hora * 60;
+
+segundos = hora * 3600;
+
+printf ("minutos = %i\n", minutos);
+printf ("segundos = %i", segundos);
+
+}
